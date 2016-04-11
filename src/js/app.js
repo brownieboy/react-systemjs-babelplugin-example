657c89936576a168644422ee3e730f7a055ef397
Michael Brown <brownieboy@gmail.com>
2016-04-12 08:22:09 +1000

    Fixed corrupt config files

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-console.log("app.js loaded");
+console.log("app.js loaded, and is not corrupted!");
 
 import Module1Class from './modules/module1.js';
 import MyReactModule from './modules/reactmodule.js';
